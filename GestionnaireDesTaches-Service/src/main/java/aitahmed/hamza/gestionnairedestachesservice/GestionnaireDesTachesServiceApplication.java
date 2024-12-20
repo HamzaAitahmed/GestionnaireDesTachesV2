c30f571efde06f62452a179f7d0108e7b048d826
HamzaAitahmed <hamzaaitahmed03@gmail.com>
2024-12-20 20:01:42 +0100

    BackEnd En Cours ...

---
+++ GestionnaireDesTaches-Service/src/main/java/aitahmed/hamza/gestionnairedestachesservice/GestionnaireDesTachesServiceApplication.java
@@ -0,0 +1,13 @@
+package aitahmed.hamza.gestionnairedestachesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionnaireDesTachesServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionnaireDesTachesServiceApplication.class, args);
+    }
+
+}
