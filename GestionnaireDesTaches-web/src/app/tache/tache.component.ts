70b4b90faf21050a2fda1878444b5f09898ce709
HamzaAitahmed <hamzaaitahmed03@gmail.com>
2024-12-23 19:51:01 +0100

    Ajouter Composent Tache

---
+++ GestionnaireDesTaches-web/src/app/tache/tache.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tache',
+  standalone: false,
+  
+  templateUrl: './tache.component.html',
+  styleUrl: './tache.component.css'
+})
+export class TacheComponent {
+
+}
