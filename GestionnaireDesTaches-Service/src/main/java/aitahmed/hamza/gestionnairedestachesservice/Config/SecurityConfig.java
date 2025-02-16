d070c8de4d51ae18065e9d465244b02a99e9ba16
HamzaAitahmed <hamzaaitahmed03@gmail.com>
2025-02-16 19:46:55 +0100

    Nouvelle Configuration de security

--- GestionnaireDesTaches-Service/src/main/java/aitahmed/hamza/gestionnairedestachesservice/Config/SecurityConfig.java
+++ GestionnaireDesTaches-Service/src/main/java/aitahmed/hamza/gestionnairedestachesservice/Config/SecurityConfig.java
@@ -1,39 +1,69 @@
-package aitahmed.hamza.gestionnairedestachesservice.Config;
+package aitahmed.hamza.gestionnairedestachesservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-//    public AuthenticationManager authenticationManager(HttpSecurity http, NoOpPasswordEncoder noOpPasswordEncoder)
-//            throws Exception {
-//        AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-//        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(noOpPasswordEncoder);
-//        return authenticationManagerBuilder.build();
+//    private final JwtAuthConverter jwtAuthConverter;
+//
+//    public SecurityConfig(JwtAuthConverter jwtAuthConverter) {
+//        this.jwtAuthConverter = jwtAuthConverter;
 //    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, HttpSecurity httpSecurity) throws Exception {
-
-        http.csrf( cs -> cs.disable() );
-
-        System.out.println("Security Config ligne 50");
-
-        return http.build();
+        return http
+                .cors(Customizer.withDefaults())
+//                .cors(cs->cs.disable())
+                .sessionManagement(sm->sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(cs->cs.disable())
+                .headers(h->h.frameOptions(fo->fo.disable()))
+                .authorizeHttpRequests(ar->ar.requestMatchers("/**").permitAll())
+                .authorizeHttpRequests(ar->ar.anyRequest().authenticated())
+//                .oauth2ResourceServer(ors->ors.jwt(jwt-> jwt.jwtAuthenticationConverter(jwtAuthConverter)))
+                .build();
     }
 
-    @SuppressWarnings("deprecation")
+
     @Bean
-    public NoOpPasswordEncoder passwordEncoder() {
-        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    CorsConfigurationSource corsConfigurationSource() {
+//        return new UrlBasedCorsConfigurationSource();
+
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setExposedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
